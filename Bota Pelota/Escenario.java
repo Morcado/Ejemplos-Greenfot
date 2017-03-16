ee1d0622e7e6dbbc0193fec96e2da0d44fd5ba35
Morcado <gposcarr18@hotmail.com>
2017-03-15 23:32:31 -0600

    Pelota con click

--- Bota Pelota/Escenario.java
+++ Bota Pelota/Escenario.java
@@ -1,19 +1,21 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-public class Escenario extends World{
+import java.util.LinkedList;
 
-    /**
-     * Constructor for objects of class MyWorld.
-     * 
-     */
+public class Escenario extends World{
+    LinkedList<Pelota> pelotas;
     public Escenario(){    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1, true); 
+        super(600, 400, 1, true);
+       
+        pelotas = new LinkedList<Pelota>();
     }
     
     @Override
     public void act(){
         if(Greenfoot.mouseClicked(this)){ 
             MouseInfo raton = Greenfoot.getMouseInfo();
+            
+            super.addObject(new Pelota(), raton.getX(), raton.getY());
             System.out.println("Coordenadas x = " + raton.getX() + ", y = " + raton.getY());
         }
     }
